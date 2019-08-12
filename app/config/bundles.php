826c53280f4bbc9b453bc2760943e2aec0b17b6f
Balazs Csaba <csaba.balazs@evozon.com>
2019-08-12 12:50:42 +0300

    Added SF webserver as a dev dependency and updated circleci conf to upload coverage via bash

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
